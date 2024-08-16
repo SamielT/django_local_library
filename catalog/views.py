efee20e544e97fb3b9ebc3c45a317203c149b9d4
SamielT <samielthai@gmail.com>
2024-08-16 14:47:26 -0600

    Add session counter on index

--- catalog/views.py
+++ catalog/views.py
@@ -17,6 +17,11 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
     # Initialize counts for books and genres containing the word
     num_books_containing_word = 0
     books_containing_word = []
@@ -33,6 +38,7 @@ def index(request):
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genres': num_genres,
+        'num_visits': num_visits,
         'num_books_containing_word': num_books_containing_word,
         'particular_word': particular_word,
         'books_containing_word': books_containing_word,
